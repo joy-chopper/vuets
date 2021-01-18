6e44f4f16b7f6d7d29bbee22b1037740f4cd026b
joy-chopper <32862436+joy-chopper@users.noreply.github.com>
2021-01-18 16:01:09 +0800

    vue3.0+typescript

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,39 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <div>ssss</div>
+    <div>{{msg}}</div>
+    <div v-for="item of list" :key="item">
+      {{item}}
+    </div>
+    <section>
+            <video src="blob:https://jx.123ku.com/f28b565d-6ccb-4d50-9640-3f752be02db1"></video>
+        <video class="dplayer-video dplayer-video-current" webkit-playsinline="" x-webkit-airplay="allow" playsinline="" preload="metadata" src="blob:https://jx.123ku.com/202d1755-f186-4bed-bfce-cf143d414927"></video>
+    </section>
+    <section>
+      <a href="blob:https://jx.123ku.com/202d1755-f186-4bed-bfce-cf143d414927">download</a>
+    </section>
+    <router-view/>
   </div>
-  <router-view/>
+  <!-- <router-view/> -->
 </template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from 'vue'
+
+// export default class Home extends Vue {
+export default defineComponent({
+  name: 'App',
+  setUp() {
+    const msg = ref('aaa')
+    const list = ref(['1', '2', '3'])
+    return {
+      list,
+      msg
+    }
+  }
+})
+</script>
 
 <style lang="less">
 #app {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <div>{{ msg }}</div>
+    <div v-for="item of list" :key="item">
+      {{item}}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, reactive, ref } from 'vue'
 
-@Options({
-  components: {
-    HelloWorld,
-  },
+// export default class Home extends Vue {
+export default defineComponent({
+  name: 'App',
+  setUp() {
+    const msg = ref('aaa')
+    const list = ref(['1', '2', '3'])
+    return {list,msg}
+  }
 })
-export default class Home extends Vue {}
 </script>
